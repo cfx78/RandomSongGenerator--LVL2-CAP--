0f9e8e8b2a47c2145c9883cce0320cfea40b096c
cfx78 <104265674+cfx78@users.noreply.github.com>
2022-11-08 11:58:27 -0600

    trying to add refresh token

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ function selectedTrack() {
 	var myHeaders = new Headers();
 	myHeaders.append(
 		'Authorization',
-		'Bearer BQA8izkZsi74nKqHfg-07gakuluilcnbHwSsddcNMFzL_ktumK4UkYBqKKBh5xxSziY_m7UV9f56WlmNj05-Va78WaChhHJ2qOHpShS98LcKRWZSYDnK_OQhbWPlKYHoKNYEuzsWpIaXeimX3KTTIeYH9UMTCwrVxsXoxIV74__fNIxQviEpZJ4dO45ZeYFpb06MrFJa92qhrmq3_BPqofI3',
+		'Bearer BQDKuysKedBIc5Hv05wOwDooNl-o8oJ0BbD8uMii4qp6bJSE0Lw7N40caC-6yw6pRpgh0L4QuSLg97cpfr03yV_-_J3828dbvZWLDgoHSpGeNiGuDLdsdrya8XsLhx8DhFAIDvTh_xqC1GC4sdhfmED7CVkCPLFl1l-eKpdplDKJ8Ln8YW_CuXxt3NOFiEjBeQCzmIGJ7JW6pM38XN3hlt5f',
 	);
 
 	var requestOptions = {
@@ -18,7 +18,7 @@ function selectedTrack() {
 	};
 
 	fetch(
-		'https://api.spotify.com/v1/playlists/0rTZ09SJnW4lqGGU2pATLB?fields=tracks.items(track.id)',
+		'https://api.spotify.com/v1/playlists/0rTZ09SJnW4lqGGU2pATLB?fields=tracks.items(track.id)&refresh_token=AQAs3YCqLtTxBVrTlIUWrJGJwOPbidDs1qSvOYIZjYJMjNY_zw_XnHrQKQq8VxPSROEv4HaJ2Sveu46EPBxiaLykeGQ1gXDsLMwrkT_NP4sipZNMkqKMEbqa5seowyfGOhs',
 		requestOptions,
 	)
 		.then((response) => response.json())
