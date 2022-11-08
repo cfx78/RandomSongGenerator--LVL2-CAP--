0831ced68fdb85958c2b6f2a76fa00743fe8890d
cfx78 <104265674+cfx78@users.noreply.github.com>
2022-11-08 17:02:48 -0600

    added some styling, still need of much more

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
-// $(function () {
-// 	$('#change').click(function () {
-// 		$('#test').load('data.txt');
-// 	});
-// });
-
 function selectedTrack() {
 	var myHeaders = new Headers();
 	myHeaders.append(
 		'Authorization',
-		'Bearer BQDKuysKedBIc5Hv05wOwDooNl-o8oJ0BbD8uMii4qp6bJSE0Lw7N40caC-6yw6pRpgh0L4QuSLg97cpfr03yV_-_J3828dbvZWLDgoHSpGeNiGuDLdsdrya8XsLhx8DhFAIDvTh_xqC1GC4sdhfmED7CVkCPLFl1l-eKpdplDKJ8Ln8YW_CuXxt3NOFiEjBeQCzmIGJ7JW6pM38XN3hlt5f',
+		'Bearer BQC3jRQmoT8Fb1Fh7yRShjiEcPg-Z9rMHJMfatutc4-DFsTI3RYxqlk_LuZo4eH3SHN_7uJhAEmnEpKvJi3x6EFNUV5tH_pIjhU6C94x6CI8LKo3LCFiqE0lkWZncTwiSggh9nxDmVzZPL_kg6DR9jHPlP81Qi34Nly0WvDfkERnOLQX50nMFN4muXNSirLb0gyNyLpRcoPvX-zNNCquDakt',
 	);
 
 	var requestOptions = {
@@ -18,7 +12,7 @@ function selectedTrack() {
 	};
 
 	fetch(
-		'https://api.spotify.com/v1/playlists/0rTZ09SJnW4lqGGU2pATLB?fields=tracks.items(track.id)&refresh_token=AQAs3YCqLtTxBVrTlIUWrJGJwOPbidDs1qSvOYIZjYJMjNY_zw_XnHrQKQq8VxPSROEv4HaJ2Sveu46EPBxiaLykeGQ1gXDsLMwrkT_NP4sipZNMkqKMEbqa5seowyfGOhs',
+		'https://api.spotify.com/v1/playlists/0rTZ09SJnW4lqGGU2pATLB?fields=tracks.items(track.id)&refresh_token=AQB6NATJ8y6XjAaFzcb535SwNeaPmtcmkGeku9akT-Ck5S-Q2piArD9p_JvIADB06QJStNfLM5DbuhAV3DFmB9rwoSp4ooXjN7FM_-Grj-APgkyfUpurVS2xRTwyXDaOP4U',
 		requestOptions,
 	)
 		.then((response) => response.json())
@@ -31,24 +25,13 @@ function selectedTrack() {
 				getTracks[Math.floor(Math.random() * getTracks.length)];
 			let baseUrl = 'https://open.spotify.com/embed/track/';
 			let endURl = '?utm_source=generator';
+
 			$('#player').attr('src', baseUrl + randomTrack + endURl);
-
-			// document.getElementById('player').innerHTML =
-			// 	'<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/1nXZnTALNXiPlvXotqHm66?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>';
 		})
-
-		// 	let baseUrl = 'https://api.spotify.com/v1/tracks/';
-		// 	return fetch(baseUrl + randomTrack, requestOptions);
-		// })
-		// .then((response) => response.json())
-		// .then((track) => console.log(track))
 		.catch((error) => console.log('error', error));
 }
 
 document.getElementById('button').addEventListener('click', selectedTrack);
-
-// selectedTrack();
-
-// $('button').click(function () {
-// 	$('printIds').load(response);
-// });
+$(document).ready(function () {
+	$('.spinner-border').hide();
+});
