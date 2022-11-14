c822a68e4aac41904d98d32e727e6bcb5db28cd6
cfx78 <104265674+cfx78@users.noreply.github.com>
2022-11-14 14:50:59 -0600

    Cleaned Up unused code, refreshed token, added small styling,removed redundant code

--- app.js
+++ app.js
@@ -15,24 +15,22 @@
 	};
 })(jQuery);
 
-// $('form').slideUp();
-
 // function to get the random song
+
 function selectedTrack() {
-	var myHeaders = new Headers();
+	let myHeaders = new Headers();
 	myHeaders.append(
 		'Authorization',
-		'Bearer BQDzWT5FwdfM2uWfrbUa2icUXfced0WvEDBd9GVDuXTNTBXyYR2g_3F0_snquT5ylbmQlekxubpt80LE_0Q4W6vdVTH6f_XTl3nkiEZeFAa89n8izMzVNs3GFCs4nkF9xr-_WYRuhibqFpmuCFihnlcBNP1oLEJ3ScYaZzw12_yCpftatc0P6eYorwDMEpt5IpvTYJ5re6PdSyO-ed0_Qvej',
+		'Bearer BQAdwwDXWrLHCcZ1d1MsdQ1asHy1LGrVlAbm5MILcyXR1LGIvEOuHBCxAjvrX7fBW_cGG9nn-hdAQiu1wI4-CeknSrWUo5cGsSKPMNoW9iqQvSlUTwDrMD6JJOGpMk1npsLumqDgFLcnOtkp3dgFQxDsnjSHzB4VyBNdZ2iMSuCT4gqkl_DzfgUCG8_OkOXmfJns7FtipJYlb-9VDOFRdO7y',
 	);
 
-	var requestOptions = {
+	let requestOptions = {
 		method: 'GET',
 		headers: myHeaders,
-		redirect: 'follow',
 	};
 
 	fetch(
-		'https://api.spotify.com/v1/playlists/0rTZ09SJnW4lqGGU2pATLB?fields=tracks.items(track.id)&refresh_token=AQB6NATJ8y6XjAaFzcb535SwNeaPmtcmkGeku9akT-Ck5S-Q2piArD9p_JvIADB06QJStNfLM5DbuhAV3DFmB9rwoSp4ooXjN7FM_-Grj-APgkyfUpurVS2xRTwyXDaOP4U',
+		'https://api.spotify.com/v1/playlists/0rTZ09SJnW4lqGGU2pATLB?fields=tracks.items(track.id)',
 		requestOptions,
 	)
 		.then((response) => response.json())
@@ -43,10 +41,13 @@ function selectedTrack() {
 			});
 			let randomTrack =
 				getTracks[Math.floor(Math.random() * getTracks.length)];
-			let baseUrl = 'https://open.spotify.com/embed/track/';
-			let endURl = '?utm_source=generator';
 
-			$('#player').attr('src', baseUrl + randomTrack + endURl);
+			$('#player').attr(
+				'src',
+				'https://open.spotify.com/embed/track/' +
+					randomTrack +
+					'?utm_source=generator',
+			);
 		})
 		.catch((error) => console.log('error', error));
 }
@@ -60,22 +61,16 @@ $(document).ready(function () {
 });
 
 // Inserting user input to alert box
-// let user = document.getElementById('user-name');
-// let submit = document.getElementById('submit');
 
-// submit.addEventListener('click', function () {
-// 	greet.innerHTML = 'Hi' + user.value;
-
-// });
 $(function () {
 	$('#btnShow').click(function () {
 		let alert = document.getElementById('alert');
-		let name = $('#txtName').val();
+		let name = $('#user-name').val();
 		$('form').remove();
-
-		alert.innerHTML =
-			'<strong>' +
-			name +
-			'</strong> If you wanna know why he is dancing in the background, just click the button below!(You should get rid of me first though...)<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>';
+		$(alert).html(
+			'<strong>Hi ' +
+				name +
+				'!</strong> If you wanna know why he is dancing in the background, just click the button below! (You should get rid of me first though...)<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+		);
 	});
 });
